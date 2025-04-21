035965deafb80255bbd7b73126ffb26627d09c98
Rex <your_username@users.noreply.github.com>
2025-04-21 21:01:02 +0530

    version-1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Appbar } from './components/Appbar'
 import { Footer } from './components/Footer'
 
 import { Hero } from './components/Hero'
-import { List } from './components/List'
+import { List, products } from './components/List'
+
 
 function App() {
 
@@ -15,13 +16,20 @@ function App() {
       
       <div className="flex flex-col gap-4 mt-16">
         <h2 className='text-white text-pretty font-semibold '>Recent Projects</h2>
-        <List description="Blogging platform, featuring user authentication (signup/login) & user-friendly interface." target="https://blog.crackedxdev.store/" project="Medium-clone" thumb="Blog.webp" tech1="React" tech2="typescript" tech3="tailwindcss" tech4="Prisma" tech5="Nodejs"/>
-        <List description="YT-clone is a video-sharing platform where users can watch, like, comment, and share videos" target="https://video.crackedxdev.store/" project="Youtube Clone frontend" thumb="ytclone.webp" tech1="Nextjs" tech2="typescript" tech3="tailwindcss" tech4="Nodejs" tech5="Vercel"/>
-        <List description="A content based recommendation system using
- NLP Models (Bag of words and TF-IDF)" target="https://github.com/csshankar/Ai_Mafia/blob/main/recommendation.ipynb" project="Apparel Product Recommendation Engine" thumb="recommendation.webp" tech1="Python" tech2="Bow" tech3="TF-IDF" tech4="Word2Vec" tech5="NLP"/>
-        <List description="Applied KNN classification algorithm in face recognition using opencv and HaarCascades" target="https://github.com/csshankar/Ai_Mafia/blob/main/face_recognition.py" project="Face Recognition System" thumb="face.webp" tech1="Python" tech2="opencv" tech3="HaarCascades" tech4="KNN" tech5="CNN"/>
-        <List description="A simple Todo app that allows users to add and view todos." target="https://github.com/csshankar/todos" project="Todo-app" thumb="todos.webp" tech1="React" tech2="typescript" tech3="tailwindcss" tech4="Express" tech5="MongoDB"/>
-
+        {products.map((product, index) => (
+          <List
+            key={index}
+            description={product.description}
+            target={product.target}
+            project={product.project}
+            thumb={product.thumb}
+            tech1={product.tech1}
+            tech2={product.tech2}
+            tech3={product.tech3}
+            tech4={product.tech4}
+            tech5={product.tech5}
+          />
+        ))}
       </div>
      
       <Footer />
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,76 @@
 
-export const List = (props:any) => {
+export const products = [
+    {
+      "description": "Payments app with features to add money and perform peer-to-peer (P2P) payments through wallet",
+      "target": "https://payments.crackedxdev.store",
+      "project": "Payment-App",
+      "thumb": "payments.webp",
+      "tech1": "Nextjs",
+      "tech2": "typescript",
+      "tech3": "tailwindcss",
+      "tech4": "Nodejs",
+      "tech5": "Postgres"
+    },
+    {
+      "description": "Blogging platform, featuring user authentication (signup/login) & user-friendly interface.",
+      "target": "https://blog.crackedxdev.store/",
+      "project": "Blog-Me",
+      "thumb": "Blog.webp",
+      "tech1": "React",
+      "tech2": "typescript",
+      "tech3": "tailwindcss",
+      "tech4": "Prisma",
+      "tech5": "Nodejs"
+    },
+    {
+      "description": "YT-clone is a video-sharing platform where users can watch, like, comment, and share videos",
+      "target": "https://video.crackedxdev.store/",
+      "project": "Youtube Clone frontend",
+      "thumb": "ytclone.webp",
+      "tech1": "Nextjs",
+      "tech2": "typescript",
+      "tech3": "tailwindcss",
+      "tech4": "Nodejs",
+      "tech5": "Vercel"
+    },
+    {
+      "description": "A content based recommendation system using\n NLP Models (Bag of words and TF-IDF)",
+      "target": "https://github.com/csshankar/Ai_Mafia/blob/main/recommendation.ipynb",
+      "project": "Apparel Product Recommendation Engine",
+      "thumb": "recommendation.webp",
+      "tech1": "Python",
+      "tech2": "Bow",
+      "tech3": "TF-IDF",
+      "tech4": "Word2Vec",
+      "tech5": "NLP"
+    },
+    {
+      "description": "Applied KNN classification algorithm in face recognition using opencv and HaarCascades",
+      "target": "https://github.com/csshankar/Ai_Mafia/blob/main/face_recognition.py",
+      "project": "Face Recognition System",
+      "thumb": "face.webp",
+      "tech1": "Python",
+      "tech2": "opencv",
+      "tech3": "HaarCascades",
+      "tech4": "KNN",
+      "tech5": "CNN"
+    }
+
+]
+
+export interface Product {
+    description: string;
+    target: string;
+    project: string;
+    thumb: string;
+    tech1: string;
+    tech2: string;
+    tech3: string;
+    tech4: string;
+    tech5: string;
+  }
+
+export const List: React.FC<Product> = (props) => {
     return (
         <div>
             <div >
@@ -8,9 +79,9 @@ export const List = (props:any) => {
                     <img src={props.thumb} alt="Project" className="rounded-xl md:w-56 w-full md:h-32 h-40 object-cover" />
                     <div>
                         <div className="flex justify-start">
-                        <a href={props.target} target="_blank" className="text-white flex items-center gap-1">
-                                
-                            <p className="text-white font-semibold text-xl ">{props.project}</p>
+                            <a href={props.target} target="_blank" className="text-white flex items-center gap-1">
+
+                                <p className="text-white font-semibold text-xl ">{props.project}</p>
                             </a>
                             <div className="flex ml-1 gap-2"><a href={props.target} target="_blank" aria-label="X" className="text-white flex items-center gap-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
@@ -39,3 +110,5 @@ export const List = (props:any) => {
         </div>
     )
 }
+
+export default List;
